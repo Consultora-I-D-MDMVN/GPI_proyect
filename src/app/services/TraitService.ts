8ce7598d783359d6a767a600024647d9e4d9bc5c
DhuertaHackmetrix <dhuerta@hackmetrix.com>
2025-06-12 23:44:08 -0400

    feat(tests): add unit tests for getTraitsByCategory function in TraitService

--- src/app/services/TraitService.ts
+++ src/app/services/TraitService.ts
@@ -3,7 +3,7 @@ import prisma from '@/utils/prisma';
 
 export const getTraitsByCategory = async (traitCategoryId: number) => {
   try {
-    const traits = await prisma.trait.findMany({
+    const traits: { label: string }[] = await prisma.trait.findMany({
       where: {
         categories: {
           some: {
@@ -17,7 +17,7 @@ export const getTraitsByCategory = async (traitCategoryId: number) => {
     });
 
     // Extraer solo los nombres (labels) y devolverlos como array de strings
-    return traits.map(trait => trait.label);
+    return traits.map((trait: { label: string }) => trait.label);
   } catch (error) {
     console.error('Error en TraitService:', error);
     throw new Error('Error obteniendo traits por categoría.');
